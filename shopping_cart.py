a04f8f90772af13d87bc01fe4d00f373366062c9
rlbgreenberg <78629622+rlbgreenberg@users.noreply.github.com>
2021-06-01 21:06:34 -0400

    product lookups

--- shopping_cart.py
+++ shopping_cart.py
@@ -53,3 +53,13 @@ while True:
     print(selected_id)
 print("WE HAVE REACHED THE END OF THE LOOP")
 print(selected_ids)
+
+# 2) Perform product lookups to determine what the product's name and price are
+#selected_ids = ["1","2","3","2","1"]
+for selected_id in selected_ids:
+    print(selected_id)
+    # lookup the corresponding product!
+    # or maybe display the selected product's name and price
+    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+    matching_product = matching_products[0]
+    print(matching_product["name"], matching_product["price"])
