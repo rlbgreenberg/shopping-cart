ec12ce89dd66b248a8271db48baaecd9e42933c5
rlbgreenberg <78629622+rlbgreenberg@users.noreply.github.com>
2021-06-10 23:01:55 -0400

    added total price

--- shopping_cart.py
+++ shopping_cart.py
@@ -38,6 +38,7 @@ def to_usd(my_price):
 # 1) capture product ids until we're done
 # (use infinite while loop)
 
+total_price = 0
 selected_ids = []
 
 
@@ -63,10 +64,11 @@ print("SELECTED PRODUCTS:")
 for selected_id in selected_ids:
     matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
     matching_product = matching_products[0]
+    total_price = total_price + matching_product["price"]
     print(matching_product["name"], matching_product["price"])
 
 print("---------------")
-print("SUBTOTAL:")
+print("SUBTOTAL: " + str(total_price))
 print("TAX:")
 print("TOTAL:")
 print("---------------")
